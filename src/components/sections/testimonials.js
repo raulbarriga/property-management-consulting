42886b7a3f717a65219f6e3c200c2f043baf17da
Raul Barriga <barrigaraulaug@gmail.com>
2024-09-07 14:47:04 -0700

    Added the other 2 testimonials. Aslo added the missing classes the example used. Need to adjust the border line and padding responsively for the 4 testimonials.

--- src/components/sections/testimonials.js
+++ src/components/sections/testimonials.js
@@ -17,17 +17,20 @@ const testimonials = () => {
         {/* What Our Client Say */}
         Trusted Expertise:
       </h2>
-      <div className="mx-auto max-w-lg md:max-w-[44rem] lg:max-w-[44rem] text-center">
-        <h2 className="font-bold text-3xl sm:text-4xl">
+      {/* second heading */}
+      <div className="mx-auto max-w-lg md:max-w-[44rem] lg:max-w-[44rem] text-center mb-6">
+        <h2 className="font-bold text-3xl sm:text-4xl px-4">
           Don&#39;t just take our word for it...
         </h2>
         <p className="mt-4 text-pretty text-body-color px-4">
           Hear from Our Clients After 20+ Years of Property Management Success
         </p>
       </div>
-      <div className="mx-auto max-w-7xl px-6 dit">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 cys dcy ded">
-          <div className="flex flex-col pb-10 sm:pb-16 dji dkm dsu">
+      {/* testimonial wrapper */}
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div id="testimonial-inner-wrap" className="mx-auto grid max-w-2xl grid-cols-1 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          {/* testimonial 1 */}
+          <div className="flex flex-col pb-10 sm:pb-16 lg:pb-0 lg:pr-8 xl:pr-20">
             <div className="relative h-12 w-[105px] self-start">
               <Image
                 alt=""
@@ -64,8 +67,82 @@ const testimonials = () => {
               </figcaption>
             </figure>
           </div>
-          {/* border-[rgb(17_24_39_/_0.1)] */}
-          <div className="flex flex-col border-t-[1px] border-black pt-10 sm:pt-16 dhj dhm dkf dko dsj">
+          {/* testimonial 2 */}
+          <div className="flex flex-col border-t-[1px] border-black pt-10 sm:pt-16 lg:border-l-[1px] lg:border-t-0 lg:pl-8 lg:pt-0 xl:pl-20">
+            <div className="relative h-12 w-[104px] self-start">
+              <Image
+                alt=""
+                fill
+                src="https://tailwindui.com/img/logos/reform-logo-gray-900.svg"
+                className="object-contain"
+              />
+            </div>
+            <figure className="mt-10 flex flex-auto flex-col justify-between">
+              <blockquote className="text-lg leading-8 text-gray-900">
+                <p>
+                  “Excepteur veniam labore ullamco eiusmod. Pariatur consequat
+                  proident duis dolore nulla veniam reprehenderit nisi officia
+                  voluptate incididunt exercitation exercitation elit. Nostrud
+                  veniam sint dolor nisi ullamco.”
+                </p>
+              </blockquote>
+              <figcaption className="mt-10 flex items-center gap-x-6">
+                <div className="relative h-14 w-14">
+                  <Image
+                    alt=""
+                    fill
+                    src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
+                    className="object-cover rounded-full bg-gray-50"
+                  />
+                </div>
+                <div className="text-base">
+                  <div className="font-semibold text-gray-900">
+                    Joseph Rodriguez
+                  </div>
+                  <div className="mt-1 text-gray-500">CEO of Reform</div>
+                </div>
+              </figcaption>
+            </figure>
+          </div>
+          {/* testimonial 3 */}
+          <div className="flex flex-col border-t-[1px] border-black pt-10 sm:pt-16 lg:border-l-[1px] lg:border-t-0 lg:pl-8 lg:pt-0 xl:pl-20">
+            <div className="relative h-12 w-[104px] self-start">
+              <Image
+                alt=""
+                fill
+                src="https://tailwindui.com/img/logos/reform-logo-gray-900.svg"
+                className="object-contain"
+              />
+            </div>
+            <figure className="mt-10 flex flex-auto flex-col justify-between">
+              <blockquote className="text-lg leading-8 text-gray-900">
+                <p>
+                  “Excepteur veniam labore ullamco eiusmod. Pariatur consequat
+                  proident duis dolore nulla veniam reprehenderit nisi officia
+                  voluptate incididunt exercitation exercitation elit. Nostrud
+                  veniam sint dolor nisi ullamco.”
+                </p>
+              </blockquote>
+              <figcaption className="mt-10 flex items-center gap-x-6">
+                <div className="relative h-14 w-14">
+                  <Image
+                    alt=""
+                    fill
+                    src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80"
+                    className="object-cover rounded-full bg-gray-50"
+                  />
+                </div>
+                <div className="text-base">
+                  <div className="font-semibold text-gray-900">
+                    Joseph Rodriguez
+                  </div>
+                  <div className="mt-1 text-gray-500">CEO of Reform</div>
+                </div>
+              </figcaption>
+            </figure>
+          </div>
+          {/* testimonial 4 */}
+          <div className="flex flex-col border-t-[1px] border-black pt-10 sm:pt-16 lg:border-l-[1px] lg:border-t-0 lg:pl-8 lg:pt-0 xl:pl-20">
             <div className="relative h-12 w-[104px] self-start">
               <Image
                 alt=""
