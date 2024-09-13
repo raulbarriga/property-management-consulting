7e079c463009ec60d9f08388ba6e0048713fbe47
Raul Barriga <barrigaraulaug@gmail.com>
2024-09-13 12:14:51 -0700

    Fixed an iframe attribute naem.

--- src/components/sections/googleMaps.js
+++ src/components/sections/googleMaps.js
@@ -11,7 +11,7 @@ const googleMaps = () => {
         aria-hidden="false"
         tabIndex="0"
         loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
+        referrerPolicy="no-referrer-when-downgrade"
       ></iframe>
     </section>
   );
